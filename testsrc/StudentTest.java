9ed9fc7cd8cfe3217d3b0a7a76ae2d43c7e60c67
Felix Delos Santos <f.delossantosiii1@nuigalway.ie>
2016-09-30 19:22:38 +0100

    Updated unit test for getusername

--- testsrc/StudentTest.java
+++ testsrc/StudentTest.java
@@ -2,6 +2,7 @@
 /**
  * Created by FelixDSantos on 28/09/2016.
  */
+import com.felix.ct417.Course;
 import com.felix.ct417.Student;
 import org.joda.time.LocalDate;
 
@@ -11,7 +12,8 @@ import static org.junit.Assert.*;
 public class StudentTest {
     @org.junit.Test
     public void getUsername() throws Exception {
-        Student student = new Student("Jim","Bob",new LocalDate("1996-03-15"), 13376931);
+        Course csit= new Course("Computer Science and Information Technology",new LocalDate("2016-09-05") ,new LocalDate("2020-05-10"));
+        Student student = new Student("Jim","Bob",new LocalDate("1996-03-15"), 13376931, csit);
 
         String studentUsername=student.getUsername(student.getFirstName(),student.getLastName(), student.getAge());
         assertEquals(studentUsername,"JimBob20");
